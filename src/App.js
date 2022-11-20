8a2003265e5e5a1fc923434c94f038bbbc273861
Nick <nicknyaga@icloud.com>
2022-11-20 22:21:15 +0300

    react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from './components/Nav';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Nav />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         My first react app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </header>
     </div>
+    </Router>
+    
   );
 }
 
